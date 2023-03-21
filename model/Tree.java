5ce6db7da7b77a4efa8cb8da5c5e79f738c19d33
Kurt Mueller <kmuel4@brockport.edu>
2023-03-21 13:45:48 -0400

    Update Tree.java

--- model/Tree.java
+++ model/Tree.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Tree {
-    //this is an edit in my branch
+import impresario.IModel;
+import impresario.IView;
+
+import javafx.scene.Scene;
+import userinterface.View;
+import userinterface.ViewFactory;
+
+public class Tree extends TLC implements IView, IModel  {
+    //tree can update, remove, and add a tree
+    //tree can update, add a tree type
+
+    private static final String myTableName = "Tree";
+    protected Properties dependancies;
+
+    private String updateStatusMessage = "";
+
+    //empty contrsuctor
+    public Tree(){
+        super(myTableName);
+
+        persistantState = new Properties();  
+    }
+
+    //constructor for existing tree
+    public Tree(int treeBARCODE) throws InvalidPrimaryKeyException {
+        super(myTableName);
+
+        setDependencies();
+        String query = "SELECT * FROM " + myTableName + " WHERE (treeBARCODE = " + treeBARCODE + ")";
+        
+        //must get a tree
+        
+
+    }
+
 }
