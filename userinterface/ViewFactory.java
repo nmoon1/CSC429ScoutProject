332bec8ee31fb948ccb46e535e8de3d66bae53e0
Nick Moon <njmoon111@gmail.com>
2023-03-28 11:49:16 -0400

    feat: Main class, TLC + TLCView

--- userinterface/ViewFactory.java
+++ userinterface/ViewFactory.java
@@ -7,7 +7,7 @@ public class ViewFactory {
 	public static View createView(String viewName, IModel model)
 	{
 		switch (viewName) {
-
+			case "TLCView": return new TLCView(model);
 		}
 		return null;
 	}
