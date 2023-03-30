fe8eda5b7236139a2790d797792a2111981006f5
Kurt Mueller <kmuel4@brockport.edu>
2023-03-30 15:06:53 -0400

    add tree view opens
    add tree view opens, but submission to database and error/success messages are missing

--- model/AddTreeAction.java
+++ model/AddTreeAction.java
@@ -1,5 +1,60 @@
 package model;
 
-public class AddTreeAction {
-    
+import java.util.Properties;
+import javafx.scene.Scene;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+public class AddTreeAction extends Action {
+
+    public AddTreeAction() throws Exception {
+        super();
+    }
+
+    protected void setDependencies() {
+        dependencies = new Properties();
+        dependencies.setProperty("Done", "CompleteAction");
+        dependencies.setProperty("Cancel", "CancelAction");
+        myRegistry.setDependencies(dependencies);
+    }
+
+    protected Scene createView() {
+        Scene currentScene = myViews.get("AddTreeActionView");
+        if(currentScene == null) {
+            // create initial view
+            View initialView = ViewFactory.createView("AddTreeActionView", this);
+            currentScene = new Scene(initialView);
+            myViews.put("AddTreeActionView", currentScene);
+        }
+        return currentScene;
+    }
+
+    public Object getState(String key) {
+        switch(key) {
+            default: return null;
+        }
+    }
+
+    public void stateChangeRequest(String key, Object value) {
+        switch(key) {
+            case "DoYourJob": 
+                doYourJob();
+                break;
+            case "StartSession":
+                startSession((Properties)value);
+                break;
+        }
+        myRegistry.updateSubscribers(key, this);
+    }
+
+    public void startSession(Properties props) {
+        try {
+            Session session = new Session(props);
+            System.out.println(session);
+            session.save();
+        } catch(Exception e) {
+            System.out.println("Error starting session: " + e.toString());
+        }
+    }
 }
