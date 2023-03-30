e8248473fbdd1f3dd6df9fa00bae0379665c2359
Nick Moon <njmoon111@gmail.com>
2023-03-30 12:48:43 -0400

    feat(wip): start shift

--- model/Session.java
+++ model/Session.java
@@ -1,5 +1,95 @@
 package model;
 
-public class Session {
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+import impresario.IView;
+
+
+public class Session extends EntityBase implements IView {
+
+    private static final String myTableName = "Session";
+    protected Properties dependencies;
+    private String updateStatusMessage = "";
+
+    public Session(Properties props) {
+        super(myTableName);
+        setDependencies();
+        persistentState = new Properties();
+		Enumeration allKeys = props.propertyNames();
+		while (allKeys.hasMoreElements() == true) {
+			String nextKey = (String)allKeys.nextElement();
+			String nextValue = props.getProperty(nextKey);
+
+			if (nextValue != null) {
+				persistentState.setProperty(nextKey, nextValue);
+			}
+		}
+    }
+
+    private void setDependencies() {
+        dependencies = new Properties();
+        myRegistry.setDependencies(dependencies);
+    }
+
+    public Object getState(String key) {
+        if(key.equals("UpdateStatusMessage")) return updateStatusMessage;
+        return persistentState.getProperty(key);
+    }
+
+    public void stateChangeRequest(String key, Object value) {
+        myRegistry.updateSubscribers(key, this);
+    }
+
+    public void updateState(String key, Object value) {
+        stateChangeRequest(key, value);
+    }
+
+    protected void initializeSchema(String tableName) {
+        if(mySchema == null) {
+            mySchema = getSchemaInfo(tableName);
+        }
+    }
+
+    public void save() {
+        updateStateInDatabase();
+    }
+
+    private void updateStateInDatabase() {
+        try {
+            String sessionID = persistentState.getProperty("ID");
+            boolean sessionExists = sessionID != null;
+            if(sessionExists) {
+                // update
+                Properties whereClause = new Properties();
+                whereClause.setProperty("ID", sessionID);
+                updatePersistentState(mySchema, persistentState, whereClause);
+                String startDate = (String)getState("StartDate");
+                String startTime = (String)getState("StartTime");
+                updateStatusMessage = "Data for session starting on " + startDate + " at " + startTime + "saved successfully";
+            } else {
+                // insert
+                Integer newSessionID = insertAutoIncrementalPersistentState(mySchema, persistentState);
+                persistentState.setProperty("ID", "" + newSessionID.intValue());
+                String startDate = (String)getState("StartDate");
+                String startTime = (String)getState("StartTime");
+                updateStatusMessage = "Data for new session starting on " + startDate + " at " + startTime + "saved successfully";
+            }
+        } catch (SQLException e) {
+            updateStatusMessage = "Error in saving session data to the database.";
+        }
+    }
+
+    // public String stringify() {
+    //     String str = "";
+    //     String id = persistentState.getProperty("ID");
+    //     String startDate = persistentState.getProperty("StartDate");
+    //     String startTime = persistentState.getProperty("StartTime");
+    //     String endTime = persistentState.getProperty("EndTime");
+    //     String startCash = persistentState.
+    //     return str;
+    // }
     
 }
