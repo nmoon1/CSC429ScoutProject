5be2fe98407c62d63b5fbea6093ef42fa82121af
Jacob Brandt <brandtjacob525@gmail.com>
2023-03-31 15:21:49 -0400

    Fixed back button and added Scout.validate(Properties)

--- model/Scout.java
+++ model/Scout.java
@@ -1,10 +1,12 @@
 package model;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
+import java.util.regex.Pattern;
 
 import exception.InvalidPrimaryKeyException;
 import impresario.IView;
@@ -59,6 +61,80 @@ public class Scout extends EntityBase implements IView
 
 			if (nextValue != null) persistentState.setProperty(nextKey, nextValue);
 		}
+	}
+	
+	public static String validate(Properties scoutInfo)
+	{
+		String firstName = scoutInfo.getProperty("FirstName"); 
+		if (firstName != null && firstName.length() == 0)
+			return "First name cannot be empty";
+    	
+		String lastName = scoutInfo.getProperty("LastName"); 
+    	if (lastName != null && lastName.length() == 0)
+    		return "Last name cannot be empty";
+    	
+    	String dateOfBirth = scoutInfo.getProperty("DateOfBirth");
+    	if (dateOfBirth != null && dateOfBirth.length() == 0)
+    		return "Date of birth cannot be empty";
+    	
+    	String phoneNumber = scoutInfo.getProperty("PhoneNumber");
+    	if (phoneNumber != null && phoneNumber.length() == 0)
+    		return "Phone number cannot be empty";
+    	
+    	String email = scoutInfo.getProperty("Email"); 
+    	if (email != null && email.length() == 0)
+    		return "Email cannot be empty";
+    	
+    	String troopID = scoutInfo.getProperty("TroopID");
+    	if (troopID.length() == 0) return "Troop ID cannot be empty";
+    	
+    	// Validate DOB format
+    	if (!Pattern.matches("^\\d{4}\\-\\d{2}\\-\\d{2}$", dateOfBirth))
+    		return "Enter date of birth as YYYY-MM-DD";
+    	
+    	// Check DOB ranges
+    	LocalDate currentDate = LocalDate.now();
+    	int month = Integer.parseInt(dateOfBirth.substring(5, 7));
+    	if (month < 1 || month > 12) return "Birth month must be between 1 and 12";
+
+    	int year = Integer.parseInt(dateOfBirth.substring(0, 4));
+    	int daysInMonth = 0;
+    	switch (month) {
+    		case 1:
+    		case 3:
+    		case 5:
+    		case 7:
+    		case 8:
+    		case 10:
+    		case 12: daysInMonth = 31; break;
+    		case 4:
+    		case 6:
+    		case 9:
+    		case 11: daysInMonth = 30; break;
+    		case 2: daysInMonth = (year % 4 == 0) ? 29 : 28; break;
+    	}
+    	int day = Integer.parseInt(dateOfBirth.substring(8));
+    	if (day < 1 || day > daysInMonth) return "Birth day must be between 1 and " + daysInMonth;
+    	
+    	// Check if DOB exceeds the current date
+    	if (year > currentDate.getYear() ||
+    		(year == currentDate.getYear() &&
+    		(month > currentDate.getMonthValue() ||
+    		(month == currentDate.getMonthValue() &&
+    		day > currentDate.getDayOfMonth()))))
+    	{
+    		return "Date of birth cannot be after current date";
+    	}
+    	
+    	// Validate phone number
+    	if (Pattern.matches("^\\d{3}\\-\\d{3}\\-\\d{4}$", phoneNumber))
+    		scoutInfo.setProperty("PhoneNumber", phoneNumber.substring(0, 3) + phoneNumber.substring(4, 7) + phoneNumber.substring(8));
+    	else if (Pattern.matches("^\\(\\d{3}\\)\\d{3}\\-\\d{4}$", phoneNumber))
+    		scoutInfo.setProperty("PhoneNumber", phoneNumber.substring(1, 4) + phoneNumber.substring(5, 8) + phoneNumber.substring(9));
+    	else if (!Pattern.matches("^\\d{10}$", phoneNumber))
+    		return "Phone number must be of the form XXXXXXXXXX or (XXX)XXX-XXXX or XXX-XXX-XXXX";
+    	
+    	return null;
 	}
 	
 	public String getFullName()
