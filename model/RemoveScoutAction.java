6fca0e62163f6c005818f87400161c8a7e92281a
Jacob Brandt <brandtjacob525@gmail.com>
2023-04-03 13:06:05 -0400

    Default Scout status to Active

--- model/RemoveScoutAction.java
+++ model/RemoveScoutAction.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Properties;
 
 import exception.InvalidPrimaryKeyException;
@@ -7,11 +8,15 @@ import javafx.scene.Scene;
 
 public class RemoveScoutAction extends Action {
 
-	private static final String viewName = "RemoveScoutView";
+	private static final String idViewName = "RemoveScoutIDView";
+	private static final String listViewName = "RemoveScoutListView";
+	private static final String confirmViewName = "RemoveScoutConfirmView";
 	private String errorMessage = "";
+	private ScoutCollection scoutCollection = null;
+	private Scout selectedScout = null;
 	
 	protected RemoveScoutAction() throws Exception {
-		super();		
+		super();
 	}
 
 	@Override
@@ -19,7 +24,7 @@ public class RemoveScoutAction extends Action {
 
 	@Override
 	protected Scene createView() {
-		return getOrCreateScene(viewName);
+		return getOrCreateScene(idViewName);
 	}
 
 	@Override
@@ -27,6 +32,8 @@ public class RemoveScoutAction extends Action {
 		switch (key) {
 			case "Error": return errorMessage;
 			case "Stage": return myStage;
+			case "ScoutList": return scoutCollection;
+			case "Scout": return selectedScout;
 		}
 		
 		return null;
@@ -41,21 +48,89 @@ public class RemoveScoutAction extends Action {
 			case "CancelAction":
 				myRegistry.updateSubscribers(key, this);
 				break;
-			case "Submit":
-				Properties scoutInfo = (Properties)value;
-				errorMessage = Scout.validate(scoutInfo);
-				if (errorMessage != null) return;
-				errorMessage = "";
-   		    	
-   		    	// Check if a scout with the same troop ID exists yet
-   		    	Scout tempScout = new Scout();
-   		    	try {
-   		    		tempScout.lookupAndStore("TroopID = '" + scoutInfo.getProperty("TroopID") + "'");
-   		    		errorMessage = "Scout with troop ID \"" + scoutInfo.getProperty("TroopID") + "\" already exists";
-   		    		return;
-   		    	} catch (InvalidPrimaryKeyException ex) { }
-   		    	
-   		    	new Scout(scoutInfo).update();
+			case "BackSearch":
+				swapToView(getOrCreateScene(idViewName));
+				break;
+			case "BackInfo":
+				swapToView(getOrCreateScene(listViewName));
+				break;
+			case "Select":
+				{
+					errorMessage = "";
+					String id = (String)value;
+					for (Scout scout : scoutCollection.scouts) {
+						if (scout.getState("ID").equals(id)) {
+							selectedScout = scout;
+							swapToView(getOrCreateScene(confirmViewName));
+							return;
+						}
+					}
+					errorMessage = "No scout with ID matching \"" + id + "\"";
+				}
+				break;
+			case "Search":
+				{
+					Properties scoutInfo = (Properties)value;
+					ArrayList<String> keys = new ArrayList<String>();
+					ArrayList<String> values = new ArrayList<String>();
+					for (String propKey : new String[] { "ID", "FirstName", "MiddleName", "LastName", "DateOfBirth", "PhoneNumber", "Email", "TroopID" }) {
+						String propValue = scoutInfo.getProperty(propKey); 
+						if (propValue.length() != 0) {
+							keys.add(propKey);
+							values.add(propValue);
+						}
+					}
+					
+					if (keys.isEmpty()) {
+						// No information entered
+						errorMessage = "Fill at least one field";
+						return;
+					}
+					
+					// Construct SQL condition string
+					String condition = "";
+					boolean first = true;
+					for (int i = 0; i < keys.size(); i++) {
+						if (!first) condition += " AND ";
+						first = false;
+						String key2 = keys.get(i);
+						condition += key2 + " = ";
+						if (key2.equals("ID")) condition += values.get(i);
+						else condition += "'" + values.get(i) + "'";
+					}
+					
+					// Retrieve scouts from database
+					scoutCollection = new ScoutCollection();
+					scoutCollection.lookupAll(condition);
+					
+					if (scoutCollection.scouts.isEmpty()) {
+						// No scouts matching given criteria
+						errorMessage = "No scouts found matching search criteria \"" + condition + "\"";
+						return;
+					}
+					
+					swapToView(getOrCreateScene(listViewName));
+				}
+				break;
+			case "Update":
+				{
+					Properties scoutInfo = (Properties)value;
+					scoutInfo.setProperty("ID", (String)selectedScout.getState("ID"));
+					errorMessage = Scout.validate(scoutInfo);
+					if (errorMessage != null) return;
+					errorMessage = "";
+	   		    	
+	   		    	// Check if a scout with the same troop ID exists yet
+	   		    	Scout tempScout = new Scout();
+	   		    	try {
+	   		    		tempScout.lookupAndStore("TroopID = '" + scoutInfo.getProperty("TroopID") + "' AND ID <> " + scoutInfo.getProperty("ID"));
+	   		    		errorMessage = "Scout with troop ID \"" + scoutInfo.getProperty("TroopID") + "\" already exists";
+	   		    		return;
+	   		    	} catch (InvalidPrimaryKeyException ex) { }
+	   		    	
+	   		    	selectedScout.setScout(scoutInfo);
+	   		    	selectedScout.update();
+				}
 				break;
 		}
 	}
