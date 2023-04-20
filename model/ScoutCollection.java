22a00ba8b8c20545650fe9072b72c41443015d46
Jacob Brandt <brandtjacob525@gmail.com>
2023-04-20 12:52:42 -0400

    Documented ScoutCollection

--- model/ScoutCollection.java
+++ model/ScoutCollection.java
@@ -12,17 +12,27 @@ public class ScoutCollection extends EntityBase implements IView
 	public Vector<Scout> scouts;
 	protected MessageView statusLog;
 	
+	/**
+	 * Creates an empty ScoutCollection
+	 */
 	public ScoutCollection()
 	{
 		super(myTableName);
 		scouts = new Vector<Scout>();
 	}
 	
+	/**
+	 * Queries the database and stores all matching Scouts
+	 * @param condition The WHERE condition of the SQL query to be sent to the database 
+	 */
 	public void lookupAll(String condition)
 	{
 		storeQueryResults("SELECT * FROM " + myTableName + " WHERE (" + condition + ")");
 	}
 	
+	/**
+	 * Retrieves and stores all Scouts in the database
+	 */
 	public void lookupAll()
 	{
 		storeQueryResults("SELECT * FROM " + myTableName);
@@ -43,6 +53,12 @@ public class ScoutCollection extends EntityBase implements IView
 		myRegistry.updateSubscribers(key, this);
 	}
 	
+	/**
+	 * Searches the ScoutCollection for a Scout with a property of name "key" matching "value" and returns the first match or null if none are found
+	 * @param key The property name to check
+	 * @param value The value the property must match
+	 * @return The first Scout matching the key and value or null if none are found
+	 */
 	public Scout getScoutByInfo(String key, String value)
 	{
 		for (int i = 0; i < scouts.size(); i++)
@@ -61,6 +77,10 @@ public class ScoutCollection extends EntityBase implements IView
 		if (mySchema == null) mySchema = getSchemaInfo(tableName);
 	}
 	
+	/**
+	 * Sends an SQL query to the database and stores all results
+	 * @param query The full SQL query to send to the database
+	 */
 	private void storeQueryResults(String query)
 	{
 		Vector allDataRetrieved = getSelectQueryResult(query);
